8c1b3e4b2e7722b5b8b0fd2dc9f0470a155356a1
cuongpham <quoccuonga2st@gmail.com>
2023-10-31 14:20:30 +0700

    Tao trang cap nhat lien he

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,28 @@
-import { createWebHistory, createRouter } from "vue-router";
+import {createWebHistory, createRouter} from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
 
 const routes = [
-	{
-		path: "/",
-		name: "contactbook",
-		component: ContactBook,
-	},
-	{
-		path: "/:pathMatch(.*)*",
-		name: "notfound",
-		component: () => import("@/views/NotFound.vue"),
-	},
+    {
+        path: "/",
+        name: "contactbook",
+        component: ContactBook,
+    },
+    {
+        path: "/contacts/:id",
+        name: "contact.edit",
+        component: () => import("@/views/ContactEdit.vue"),
+        props: true
+    },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notfound",
+        component: () => import("@/views/NotFound.vue"),
+    },
 ];
 
 const router = createRouter({
-	history: createWebHistory(import.meta.env.BASE_URL),
-	routes,
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes,
 });
 
 export default router;
